3ff2315ee599fdb55231a554cdae027d1a4facff
2022jobyrne <22jobyrne@athenian.org>
2020-11-05 05:49:15 +0000

    groceery list class final code

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,36 @@
+import java.util.Arrays;
 public class GroceryList {
-    
+    private String[] arr;
+
+    public GroceryList(){
+        this.arr = new String[10];
+    }
+    public void add(String item)
+    {
+        //String item = " ";
+        for (int i = 0; i < arr.length; i++) 
+        {
+            if (arr[i] == null) {
+				arr[i] = item;
+                break;
+            }
+        }
+    }
+    public void remove(String item)
+    {
+        //String item = " ";
+        for (int i = 0; i < arr.length; i++) 
+        {
+            if (arr[i] == item) {
+				arr[i] = null;
+                break;
+            }
+        }
+    }
+    public String toString()
+    {
+        String x = (Arrays.toString(arr));
+        return x;
+    }
+
 }
